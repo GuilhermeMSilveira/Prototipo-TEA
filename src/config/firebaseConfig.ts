36c24c1ab75521381e7c3ed6bd756e7735df85a6
Guilherme Marques Silveira <128319947+GuilhermeMSilveira@users.noreply.github.com>
2025-03-29 15:16:15 -0300

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA7W3xmnKjnnBJ7LBFiiHEM-jItJZIPDWk',
-  authDomain: 'testeuser@gmail.com',
-  projectId: 'guilhermem-29d59',
-  storageBucket: 'Teste.projetoTCC',
-  messagingSenderId: '1033451552965',
-  appId: '1:1033451552965:android:db6ae67c3953668507b0ac',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Inicializa o app Firebase
